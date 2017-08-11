98d992f7f5ecb20d44f6da45d101b2ad347b9e57
David-Rodden <davidrodden273@gmail.com>
2017-08-11 16:06:26 -0700

    Sample female and male stats collected and displayed through console

--- PTest.py
+++ PTest.py
@@ -3,12 +3,22 @@ from private_info.Login import private_login
 import re
 
 reddit = private_login()
-for submission in reddit.subreddit("amiugly").new(limit=10):
-    gender = re.search(".*(\d+)?(M|F)(\d+)?.*", submission.title)
+
+female_stats = []
+male_stats = []
+for submission in reddit.subreddit("amiugly").new(limit=20):
+    gender = re.search(".*(\d+)?(m|f)(\d+)?.*", submission.title.lower())
     if not gender:
         continue
-    print("Gender: " + gender.group(2))
     for comment in submission.comments:
         m = re.search('.*(\d+(\.\d+)).*', comment.body)
         if m:
-            print(m.group(1))
+            score = float(m.group(1))
+            print(str(score) + ": " + submission.url)
+            if score < 10:
+                if gender.group(2) == "f":
+                    female_stats.append(score)
+                else:
+                    male_stats.append(score)
+print(female_stats)
+print(male_stats)
