1a0e61ac7f9ad0bd0b17fcf358e96018f3019d71
David-Rodden <davidrodden273@gmail.com>
2017-08-11 15:52:04 -0700

    Title parsed for gender and comments parsed for score

--- PTest.py
+++ PTest.py
@@ -1,7 +1,14 @@
 import praw
 from private_info.Login import private_login
+import re
 
 reddit = private_login()
-for submission in reddit.subreddit("amiugly").new(limit=25):
+for submission in reddit.subreddit("amiugly").new(limit=10):
+    gender = re.search(".*(\d+)?(M|F)(\d+)?.*", submission.title)
+    if not gender:
+        continue
+    print("Gender: " + gender.group(2))
     for comment in submission.comments:
-        print("__________\n" + comment.body)
+        m = re.search('.*(\d+(\.\d+)).*', comment.body)
+        if m:
+            print(m.group(1))
